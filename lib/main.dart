ab07d71a734b08b08bf4994097c10255d574e2e9
Ahmed Ezz <ahmedezz9877@gmail.com>
2025-07-11 18:04:18 +0300

    Remove Bloc Observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notes_app/constants/constants.dart';
 import 'package:notes_app/models/note_model.dart';
-import 'package:notes_app/simple_observer_cubit.dart';
 import 'package:notes_app/views/no_notes_view.dart';
 import 'package:notes_app/views/notes_view.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Bloc.observer = SimpleObserverCubit();
   await Hive.initFlutter();
   Hive.registerAdapter(NoteModelAdapter());
   await Hive.openBox<NoteModel>(kNotesBox);
